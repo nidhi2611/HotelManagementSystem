127b01cf14d9702aa5c87ebd9b2ee8d5e232be2d
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-01-05 03:00:51 +0530

    attributes of a Customer

---
+++ src/hotelcustomer/Customer.java
@@ -0,0 +1,13 @@
+package hotelcustomer;
+
+public class Customer {
+    private int customerId;
+    private String customerName;
+    private int addharNumber;
+    private int customerRoomNo;
+    private long customerContact;
+    private String checkIn;
+    private String checkOut;
+
+
+}
