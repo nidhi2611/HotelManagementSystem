93ff990d759e9b2187f5918ff8360c62544e87b1
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-01-05 03:05:34 +0530

    constructor of customer class

--- src/hotelcustomer/Customer.java
+++ src/hotelcustomer/Customer.java
@@ -9,5 +9,14 @@ public class Customer {
     private String checkIn;
     private String checkOut;
 
+    public Customer(int cid, String n, int an, int rn, long c, String cin, String cout) {
+        customerId = cid;
+        customerName = n;
+        addharNumber = an;
+        customerRoomNo = rn;
+        customerContact = c;
+        checkIn = cin;
+        checkOut = cout;
+    }
 
 }
