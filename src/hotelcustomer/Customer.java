dda2df9d1fb570694c6dd72386de8922d2846d57
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-01-05 03:21:33 +0530

    final commit for customer class getter setter created

--- src/hotelcustomer/Customer.java
+++ src/hotelcustomer/Customer.java
@@ -31,4 +31,67 @@ public class Customer {
                 "amount to be paid" + amount;
     }
 
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getAddharNumber() {
+        return addharNumber;
+    }
+
+    public void setAddharNumber(int addharNumber) {
+        this.addharNumber = addharNumber;
+    }
+
+    public int getCustomerRoomNo() {
+        return customerRoomNo;
+    }
+
+    public void setCustomerRoomNo(int customerRoomNo) {
+        this.customerRoomNo = customerRoomNo;
+    }
+
+    public long getCustomerContact() {
+        return customerContact;
+    }
+
+    public void setCustomerContact(long customerContact) {
+        this.customerContact = customerContact;
+    }
+
+    public String getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public String getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
